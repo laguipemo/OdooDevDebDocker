c8b522a33c2212c2ec077c59648ced93f1bbda1e
Lázaro Guillermo Pérez Montoto <laguipemo@gmail.com>
2024-11-27 22:39:38 +0100

    Small fixes and created a compute and store field

--- custom-addons/manage/models/models.py
+++ custom-addons/manage/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import datetime, timedelta
 
 
 # class manage(models.Model):
@@ -21,14 +22,14 @@ class task(models.Model):
     _name = 'manage.task'
     _description = 'manage.task'
 
-    code = fields.Char(compute="_compute_code", store=True)
+    code = fields.Char(compute="_compute_code")
     name = fields.Char(string="Nombre", readonly=False, required=True, help="Introduzca el nombre")
     description = fields.Text()
     creation_date = fields.Date()
     start_date = fields.Datetime()
     end_date = fields.Datetime()
     is_paused = fields.Boolean()
-    sprint = fields.Many2one(comodel_name="manage.sprint", string="Sprint", help='Sprint relacionado')
+    sprint = fields.Many2one(comodel_name="manage.sprint", ondelete="cascade", string="Sprint", help='Sprint relacionado')
     technologies = fields.Many2many(comodel_name="manage.technology", 
                                     relation="manage_task_technology_rel",
                                     column1="task_id", 
@@ -50,8 +51,17 @@ class sprint(models.Model):
     name = fields.Char()
     description = fields.Text()
     start_date = fields.Datetime()
-    end_date = fields.Datetime()
+    duration = fields.Integer(string="Duración (días)")
+    end_date = fields.Datetime(compute="_compute_end_date", store=True)
     tasks = fields.One2many(comodel_name="manage.task", inverse_name="sprint", string="Tareas")
+
+    @api.depends('start_date', 'duration')
+    def _compute_end_date(self):
+        for sprint in self:
+            if sprint.duration:
+                if not sprint.start_date:
+                    sprint.start_date = datetime.now()
+                sprint.end_date = sprint.start_date + timedelta(days=sprint.duration)
 
 class technology(models.Model):
     _name = 'manage.technology'
