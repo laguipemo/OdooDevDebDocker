b4bbf243b91147119ecb4050b4bf7f0a9a77e115
Lázaro Guillermo Pérez Montoto <laguipemo@gmail.com>
2024-12-24 16:10:39 +0100

    Created the first model 'SoporteIncidencias' using some diferents fields

--- custom-addons/soporte/models/models.py
+++ custom-addons/soporte/models/models.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
+
+class SoporteIncidencias(models.Model):
+    '''Modelo para la gestión de incidencias'''
+    _name = 'soporte.incidencias'
+    _description = 'Modelo para la gestión de incidencias'
+
+    name = fields.Char(string='Nombre', required=True)
+    description = fields.Text(string='Description')
+    priority = fields.Integer(
+        string='Prioridad',
+        default=1,
+        help='Prioridad de la incidencia (1-10). Valor mayor que 7 es urgente'
+        )
+    urgent = fields.Selection(
+        string='Urgente',
+        selection=[('0', 'No'), ('1', 'Si')])
+    closed = fields.Boolean(string='Cerrada', default=False)
+
 
 
 # class soporte(models.Model):
