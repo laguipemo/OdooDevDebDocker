37feaaeec5f92a10eac11f1effa5904bafbf5603
Lázaro Guillermo Pérez Montoto <laguipemo@gmail.com>
2024-12-24 13:55:00 +0100

    First changes in __manifest__.py

--- custom-addons/soporte/__manifest__.py
+++ custom-addons/soporte/__manifest__.py
@@ -3,20 +3,19 @@
     'name': "soporte",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Módulo de soporte para la gestión de incidencias""",
 
     'description': """
-        Long description of module's purpose
+        Descripción del módulo de soporte para la gestión de incidencias
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "LGPM developments",
+    'website': "https://github.com/laguipemo",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'tools',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -24,7 +23,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
