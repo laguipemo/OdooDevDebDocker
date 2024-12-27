b885594b31b8e74dd740196aa2dfe425f7544f66
Lázaro Guillermo Pérez Montoto <laguipemo@gmail.com>
2024-12-28 00:22:23 +0100

    Created the many2many relationship between 'tecnico' and 'incidencia' models

--- custom-addons/soporte/models/soporte_tecnico.py
+++ custom-addons/soporte/models/soporte_tecnico.py
@@ -8,4 +8,12 @@ class SoporteTecnico(models.Model):
     _description = 'Modelo para almacenar las personas que trabajan en las incidencias'
 
     name = fields.Char(string='Nombre', required=True)
+    incidencia_ids = fields.Many2many(
+        string='Incidencias',
+        comodel_name='soporte.incidencia',
+        relation='soporte_incidencia_tecnico_rel',
+        column1='tecnico_id',
+        column2='incidencia_id',
+        help='Incidencias en las que trabaja el técnico'
+    )
     
