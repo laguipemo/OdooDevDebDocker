67e697cd9fd4616e78c95794923a29014ce079ea
Lázaro Guillermo Pérez Montoto <laguipemo@gmail.com>
2024-12-27 15:27:14 +0100

    More refactors, made the root manu independent

--- custom-addons/soporte/__manifest__.py
+++ custom-addons/soporte/__manifest__.py
@@ -26,6 +26,8 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/menu_root.xml',
+        'views/incidencia.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
