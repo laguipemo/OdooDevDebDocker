0e93818587b215e3fca55f6dd74f4fcfc7c82ec6
Lázaro Guillermo Pérez Montoto <laguipemo@gmail.com>
2025-01-03 17:52:58 +0100

    Adapted the 'tecnico' model to inherit from 'soprte.tecnico' and extended it with a new selection field called 'tipo'

--- custom-addons/soporteExt/models/soporte_tecnico.py
+++ custom-addons/soporteExt/models/soporte_tecnico.py
@@ -5,15 +5,18 @@ from odoo import models, fields, api
 
 class SoporteTecnico(models.Model):
     _name = 'soporte.tecnico'
+    _inherit = ['soporte.tecnico']
     _description = 'Modelo para almacenar las personas que trabajan en las incidencias'
 
-    name = fields.Char(string='Nombre', required=True)
-    incidencia_ids = fields.Many2many(
-        string='Incidencias',
-        comodel_name='soporte.incidencia',
-        relation='soporte_incidencia_tecnico_rel',
-        column1='tecnico_id',
-        column2='incidencia_id',
-        help='Incidencias en las que trabaja el técnico'
-    )
+    tipo = fields.Selection(
+        string='Tipo',
+        selection=[
+            ('0', 'Técnico General'),
+            ('1', 'Técnico Hardware'),
+            ('2', 'Técnico Software'),
+            ('3', 'Técnico Redes'),
+            ],
+        default='0',
+        )
+    
     
