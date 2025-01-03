269e1c366e6da7391e9d4f10647cfdcb785aa9fc
Lázaro Guillermo Pérez Montoto <laguipemo@gmail.com>
2025-01-03 18:45:48 +0100

    Implemented mail and activity functionalities in 'tecnico' model

--- custom-addons/soporteExt/models/soporte_tecnico.py
+++ custom-addons/soporteExt/models/soporte_tecnico.py
@@ -5,8 +5,8 @@ from odoo import models, fields, api
 
 class SoporteTecnico(models.Model):
     _name = 'soporte.tecnico'
-    _inherit = ['soporte.tecnico']
-    _description = 'Modelo para almacenar las personas que trabajan en las incidencias'
+    _inherit = ['soporte.tecnico', 'mail.thread', 'mail.activity.mixin']
+    _description = 'Modelo para almacenar las personas que trabajan en las incidencias.'
 
     tipo = fields.Selection(
         string='Tipo',
