a9ff88ec2eea74775a1347a1e760ade38736e863
Lázaro Guillermo Pérez Montoto <laguipemo@gmail.com>
2025-01-12 15:23:55 +0100

    Updated '__manifest__.py' field (name, description, data, etc)

--- custom-addons/lgpm_maintenance/__manifest__.py
+++ custom-addons/lgpm_maintenance/__manifest__.py
@@ -3,29 +3,29 @@
     'name': "lgpm_maintenance",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Extends the maintenance module with SAT verifications""",
 
     'description': """
-        Long description of module's purpose
+        The module extends the base maintenance module with SAT verifications data performed on gas extraction equipments
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "LGPM developments",
+    'website': "https://github.com/laguipemo",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Tools',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'maintenance'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
+        'views/lgpm_maintenance_sat_views.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
