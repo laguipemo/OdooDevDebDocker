b0a2178b9eb12b96e5d9fb07d0dd6b2547a771e4
Lázaro Guillermo Pérez Montoto <laguipemo@gmail.com>
2025-01-12 15:29:53 +0100

    Added 'lgpm_maintenance_sat.py' for new inherited  model

---
+++ custom-addons/lgpm_maintenance/models/lgpm_maintenance_sat.py
@@ -0,0 +1,21 @@
+#-*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class LgpmMaintenanceEquipment(models.Model):
+    _name = 'maintenance.equipment'
+    _inherit = ['maintenance.equipment']
+    _description = 'lgpm_maintenance.equipment'
+
+    equipment_type = fields.Selection(
+        string = 'Type',
+        selection=[
+            ('VG', 'Vitrina de Gases'),
+            ('PALP', 'Punto de Aspiración Localizada Pared'),
+            ('PALB', 'Punto de Aspiración Localizada Brazo'),
+            ('AS', 'Armario de seguridad'),
+            ('NO', ''),
+        ],
+        default='NO'
+    )
