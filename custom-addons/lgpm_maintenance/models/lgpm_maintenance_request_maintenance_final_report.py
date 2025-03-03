1fc82bb67fe7930c35693de552c91715faa2d96f
Lázaro Guillermo Pérez Montoto <laguipemo@gmail.com>
2025-03-03 23:18:28 +0100

    Adde new model to __init__.py and include check for equipment type, must be one type

--- custom-addons/lgpm_maintenance/models/lgpm_maintenance_request_maintenance_final_report.py
+++ custom-addons/lgpm_maintenance/models/lgpm_maintenance_request_maintenance_final_report.py
@@ -1,4 +1,5 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 class LgpmMaintenanceRequestMaintenanceFinalReport(models.AbstractModel):
     _name = 'report.lgpm_maintenance.request_maintenance_final_report'
@@ -9,6 +10,11 @@ class LgpmMaintenanceRequestMaintenanceFinalReport(models.AbstractModel):
         report = self.env['ir.actions.report']._get_report_from_name('lgpm_maintenance.request_maintenance_final_report')
         # get the records selected for this rendering of the report
         docs = self.env[report.model].browse(docids)
+
+        # raise an error if user select more than one type of equipment
+        equipment_types = set(docs.mapped('equipment_id.equipment_type'))
+        if len(equipment_types) > 1:
+            raise UserError(_('Solo se puede seleccionar mantenimientos del mismo tipo de equipo'))
         # return a custom rendering context
         return {
             'doc_ids': docids,
